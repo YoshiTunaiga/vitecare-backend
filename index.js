ef1d589d76323f1c133a6964901870fc5a9b2f91
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2024-11-26 22:27:01 -0500

    chore: added admin route

--- index.js
+++ index.js
@@ -223,6 +223,69 @@ app.get("/api/:key", (req, res) => {
   }
 });
 
+// ADMIN: GET USER APPOINTMENTS LIST
+app.get("/admin", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const appointments = userId
+      ? await databases.listDocuments(DATABASE_ID, APPOINTMENT_COLLECTION_ID, [
+          Query.equal("userId", [userId]),
+        ])
+      : await databases.listDocuments(DATABASE_ID, APPOINTMENT_COLLECTION_ID, [
+          Query.orderDesc("$createdAt"),
+        ]);
+
+    const scheduledAppointments = appointments.documents.filter(
+      (appointment) => appointment.status === "scheduled"
+    );
+
+    const pendingAppointments = appointments.documents.filter(
+      (appointment) => appointment.status === "pending"
+    );
+
+    const cancelledAppointments = appointments.documents.filter(
+      (appointment) => appointment.status === "cancelled"
+    );
+
+    let data = {
+      totalCount: appointments.total,
+      scheduledCount: scheduledAppointments.length,
+      pendingCount: pendingAppointments.length,
+      cancelledCount: cancelledAppointments.length,
+      documents: appointments.documents,
+    };
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = appointments.documents.reduce((acc, appointment) => {
+      switch (appointment.status) {
+        case "scheduled":
+          acc.scheduledCount++;
+          break;
+        case "pending":
+          acc.pendingCount++;
+          break;
+        case "cancelled":
+          acc.cancelledCount++;
+          break;
+      }
+      return acc;
+    }, initialCounts);
+
+    data.totalCount = appointments.total;
+    data = { ...data, ...counts };
+    data.documents = appointments.documents;
+
+    res.send({ apptsReturned: JSON.parse(JSON.stringify(data)) });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
