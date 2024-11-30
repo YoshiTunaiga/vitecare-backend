a968c1cd58547fc52e09a6b5c77d7086221fd151
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2024-11-29 21:21:47 -0500

    fix: app use of cors

--- index.js
+++ index.js
@@ -44,16 +44,8 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:5173"], // Configure your frontend URL
     credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      // "DELETE"
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      // "Authorization"
-    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(morgan("dev")); // Logging middleware
